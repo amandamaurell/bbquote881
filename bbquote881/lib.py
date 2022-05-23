d69cf6770a53f992b3b3a8daa7e025a964bf9cf6
Amanda Maurell <amanda.maurell@gmail.com>
2022-05-23 17:51:48 +0200

    testing CD again

--- bbquote881/lib.py
+++ bbquote881/lib.py
@@ -3,7 +3,7 @@ import requests
 def get_quote():
     url = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes'
     response = requests.get(url).json()[0]
-    return f"Quote: {response['quote']} --- author: {response['author']}"
+    return f"Quote: {response['quote']} -- author: {response['author']}"
 
 if __name__ == '__main__':
     print(get_quote())
